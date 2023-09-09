8f894b84287f005d1d85ff240c902c86a3ab4dde
Jason de Wolff <jasondewolff@gmail.com>
2023-09-09 17:51:28 +0200

    Added tree.hh dependency

--- UniverseEngine/UniverseEngine/Material.h
+++ UniverseEngine/UniverseEngine/Material.h
@@ -3,9 +3,11 @@
 #include <glm/vec4.hpp>
 
 #include "Texture.h"
+#include "Pool.h"
 
 namespace UniverseEngine {
 	struct Material {
 		glm::vec4 baseColor;
+		Handle<Texture> baseColorMap;
 	};
 }
