85cc08249082ded0d55685c8dfeec908dad0d035
Gabriël Pouw <gabriel@pouw.com>
2023-09-10 01:55:11 +0200

    Load usd file

--- UniverseEngine/UniverseEngine/USDLoader.cpp
+++ UniverseEngine/UniverseEngine/USDLoader.cpp
@@ -5,12 +5,51 @@ namespace fs = std::filesystem;
 
 #include <tinyusdz/tinyusdz.hh>
 
+// Include pprinter.hh and value-pprint.hh if you want to print TinyUSDZ classes/structs/enums.
+// `tinyusdz::to_string()` and `std::operator<<` for TinyUSDZ classes/enums are provided separately for faster compilation
+#include <iostream>
+#include <tinyusdz/pprinter.hh>
+#include <tinyusdz/value-pprint.hh>
+
 namespace UniverseEngine {
     Handle<Model> Resources::LoadUSD(const fs::path& filePath) {
 
+        std::string filename = filePath.u8string();
 
+        tinyusdz::Stage stage;  // Stage in USD terminology is nearly meant for Scene in generic 3D
+                                            // graphics terminology.
+        std::string warn;
+        std::string err;
 
+        // Auto detect USDA/USDC/USDZ
+        bool ret = tinyusdz::LoadUSDFromFile(filename, &stage, &warn, &err);
 
-        return Handle<Model>::Invalid();
+        if (warn.size()) {
+            std::cout << "WARN : " << warn << "\n";
+        }
+
+        if (!ret) {
+            if (!err.empty()) {
+                std::cerr << "ERR : " << warn << "\n";
+            }
+            return Handle<Model>::Invalid();
+        }
+
+        // Print Stage(Scene graph)
+        std::cout << tinyusdz::to_string(stage) << "\n";
+
+        // You can also use ExportToString() as done in pxrUSD
+        // std::cout << stage.ExportToString() << "\n";
+
+        // stage.metas() To get Scene metadatum,
+         for (const tinyusdz::Prim& root_prim : stage.root_prims()) {
+            std::cout << root_prim.absolute_path() << "\n";
+            // You can traverse Prim(scene graph object) using Prim::children()
+            // See examples/api_tutorial and examples/tydra_api for details.
+        }
+
+        Handle<Model> handle = this->models->Alloc();
+        //this->models->Value(handle).Value() = parsedModel;
+        return handle;
     }
 }  // namespace UniverseEngine
