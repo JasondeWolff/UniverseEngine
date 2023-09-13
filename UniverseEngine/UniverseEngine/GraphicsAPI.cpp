434593be335e11e94f4dff8acaba708533616fe8
Jason de Wolff <jasondewolff@gmail.com>
2023-09-13 15:05:57 +0200

    Added gl debug messages & removed x84 config

--- UniverseEngine/UniverseEngine/GraphicsAPI.cpp
+++ UniverseEngine/UniverseEngine/GraphicsAPI.cpp
@@ -3,9 +3,21 @@
 #ifdef GRAPHICS_API_GL
 #include <GL/glew.h>
 
+#include "Logging.h"
+
 namespace UniverseEngine {
+    void GlDebugCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length,
+                         const GLchar* message, const void* userParam) {
+        UE_FATAL("GL: %s", message);
+    }
+
     void GraphicsAPI::Init() {
         glewInit();
+
+#ifndef NDEBUG
+        glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+        glDebugMessageCallback(GlDebugCallback, nullptr);
+#endif
     }
-}
+}  // namespace UniverseEngine
 #endif
