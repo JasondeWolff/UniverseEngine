1be7a0a6987cbcededf13315b4499146aa444b80
Gabriël Pouw <gabriel@pouw.com>
2023-09-16 13:24:54 +0200

    Debug Dependecies fix

--- UniverseEngine/UniverseEngine/Plane.cpp
+++ UniverseEngine/UniverseEngine/Plane.cpp
@@ -1,11 +1,13 @@
 #include "Plane.h"
 #include "MathUtil.h"
 
+#include <algorithm> 
+
 namespace UniverseEngine{
     Plane::Plane(int width, int height, int widthSegments, int heightSegments)
         : m_Width(width), m_Height(height), m_WidthSegments(widthSegments), m_HeightSegments(heightSegments)
     {
-       /* const int width_half = width / 2;
+        const int width_half = width / 2;
         const int height_half = height / 2;
 
         const int gridX = widthSegments;
@@ -19,6 +21,9 @@ namespace UniverseEngine{
 
         name = "Plane";
 
+        vertices.clear();
+        vertices.reserve(gridX * gridY);
+
         for (int iy = 0; iy < gridY1; iy++) {
             const int y = iy * segment_height - height_half;
 
@@ -26,24 +31,32 @@ namespace UniverseEngine{
                 const int x = ix * segment_width - width_half;
 
                 Vertex v;
-                v.position = {x, 0, -y};
+                v.position = glm::vec3(x, 0, -y);
                 v.normal = Up();
-                v.texCoord = {ix / gridX, 1 - (iy / gridY)};
-                v.color = {1, 1, 1};
+                v.texCoord = glm::vec2(ix / gridX, 1 - (iy / gridY));
+                v.color = glm::vec3(1, 1, 1);
+
                 vertices.emplace_back(v);
             }
         }
 
+        indices.clear();
+        indices.reserve(gridX * gridY);
+
         for (int iy = 0; iy < gridY; iy++) {
             for (int ix = 0; ix < gridX; ix++) {
-                const int a = ix + gridX1 * iy;
-                const int b = ix + gridX1 * (iy + 1);
-                const int c = (ix + 1) + gridX1 * (iy + 1);
-                const int d = (ix + 1) + gridX1 * iy;
+                const uint32_t a = ix + gridX1 * iy;
+                const uint32_t b = ix + gridX1 * (iy + 1);
+                const uint32_t c = (ix + 1) + gridX1 * (iy + 1);
+                const uint32_t d = (ix + 1) + gridX1 * iy;
 
-                indices.emplace_back(a, b, d);
-                indices.emplace_back(b, c, d);
+                std::vector<uint32_t> tempIndices = {
+                    a, b, d,
+                    b, c, d
+                };
+                
+                std::copy(tempIndices.begin(), tempIndices.end(), indices.end());
             }
-        }*/
+        }
     }
 }
