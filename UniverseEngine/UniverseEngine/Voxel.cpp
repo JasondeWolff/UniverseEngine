d3adc61f62de354c916b4f8361758442b7f62b03
Gabriël Pouw <gabriel@pouw.com>
2023-09-20 18:14:02 +0200

    Voxel primitive

---
+++ UniverseEngine/UniverseEngine/Voxel.cpp
@@ -0,0 +1,13 @@
+#include "Voxel.h"
+
+namespace UniverseEngine {
+    Voxel::Voxel(int width, int height)
+        : m_Width(width),
+          m_Height(height){
+
+    }
+
+    Voxel::~Voxel() {
+
+    }
+}  // namespace UniverseEngine
