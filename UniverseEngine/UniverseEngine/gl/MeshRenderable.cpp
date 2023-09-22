676b35bb5ac34cde27f874e80f4ac2f2aae82ac5
Jason de Wolff <jasondewolff@gmail.com>
2023-09-22 11:28:47 +0200

    Fixed gl config

--- UniverseEngine/UniverseEngine/gl/MeshRenderable.cpp
+++ UniverseEngine/UniverseEngine/gl/MeshRenderable.cpp
@@ -10,7 +10,8 @@
 #include "GlDebugNames.h"
 
 namespace UniverseEngine {
-    MeshRenderable::MeshRenderable(const Mesh& mesh)
+    MeshRenderable::MeshRenderable(std::shared_ptr<LogicalDevice> device,
+                                   const PhysicalDevice& physicalDevice, const Mesh& mesh)
         : indexCount(static_cast<uint32_t>(mesh.indices.size())) {
         UE_ASSERT(mesh.vertices.size() > 0);
         UE_ASSERT(mesh.indices.size() > 0);
