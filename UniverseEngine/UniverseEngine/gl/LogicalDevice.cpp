7a64dc0692ffe84278411df4f3d95b963f1ae433
Jason de Wolff <jasondewolff@gmail.com>
2023-09-24 22:46:14 +0200

    Update LogicalDevice.cpp

--- UniverseEngine/UniverseEngine/gl/LogicalDevice.cpp
+++ UniverseEngine/UniverseEngine/gl/LogicalDevice.cpp
@@ -2,11 +2,14 @@
 #ifdef GRAPHICS_API_GL
 
 #include "../LogicalDevice.h"
+#include "../Logging.h"
 
 namespace UniverseEngine {
     LogicalDevice::LogicalDevice(std::shared_ptr<GraphicsInstance> instance,
                                  const PhysicalDevice& physicalDevice, bool debug)
         : instance(instance) {
+        const GLubyte* renderer = glGetString(GL_RENDERER);
+        UE_INFO("Using Device: %s", renderer);
     }
 
     LogicalDevice::~LogicalDevice() {
