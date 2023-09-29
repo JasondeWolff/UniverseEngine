b32c7a15ec07843c5b2bae1ee69c5f4cf542a284
Jason de Wolff <jasondewolff@gmail.com>
2023-09-29 18:00:24 +0200

    Fixed showcase transforms

--- UniverseEngine/Game/AssetDemo.cpp
+++ UniverseEngine/Game/AssetDemo.cpp
@@ -8,11 +8,11 @@ void AssetDemo::OnStart() {
 
 	std::vector<std::pair<std::string, Transform>> scenePaths = {
 		{"Assets/Models/Showcase/AntiqueCamera/AntiqueCamera.gltf", Transform(glm::vec3{}, EulerToQuat(Right() * 180.0f), glm::vec3(0.3f))},
-		{"Assets/Models/Showcase/Corset/Corset.gltf", Transform(glm::vec3{}, EulerToQuat(Right() * 180.0f + Forward() * 180.0f), glm::vec3(30.0f))},
-		{"Assets/Models/Showcase/DamagedHelmet/DamagedHelmet.gltf", Transform(glm::vec3{}, EulerToQuat(Up() * -90.0f + Right() * 90.0f), glm::vec3(1.0f))},
+		{"Assets/Models/Showcase/Corset/Corset.gltf", Transform(glm::vec3{}, EulerToQuat(Forward() * 180.0f), glm::vec3(30.0f))},
+		{"Assets/Models/Showcase/DamagedHelmet/DamagedHelmet.gltf", Transform(glm::vec3(0.0f, 1.0f, 0.0f), EulerToQuat(Up() * 90.0f + Right() * 90.0f), glm::vec3(1.0f))},
 		{"Assets/Models/Showcase/FlightHelmet/FlightHelmet.gltf", Transform(glm::vec3{}, EulerToQuat(glm::vec3{}), glm::vec3(2.5f))},
-		{"Assets/Models/Showcase/SciFiHelmet/SciFiHelmet.gltf", Transform{}},
-		{"Assets/Models/Showcase/Buggy/Buggy.gltf", Transform(glm::vec3{}, EulerToQuat(glm::vec3{}), glm::vec3(0.02f))},
+		{"Assets/Models/Showcase/SciFiHelmet/SciFiHelmet.gltf", Transform(glm::vec3(0.0f, 0.7f, 0.0f), EulerToQuat(glm::vec3{}), glm::vec3(0.7f))},
+		{"Assets/Models/Showcase/Buggy/Buggy.gltf", Transform(glm::vec3(0.0f, 0.1f, 0.0f), EulerToQuat(glm::vec3{}), glm::vec3(0.02f))},
 	};
 
 	glm::vec3 translation{};
